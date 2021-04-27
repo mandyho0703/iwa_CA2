e3acd500b068d09db56f5e0f58dd54a45198cfcf
mandyho0703 <mandyho0703@gmail.com>
2021-04-27 23:01:35 +0000

    axios set up

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require('http'),
+logger = require('morgan'),
+cors = require('cors'),
+express = require('express'),
+bodyParser = require('body-parser'),
+mongoose = require('mongoose'),
+dotenv = require("dotenv");
+
+var app = express();
+var port = process.env.PORT || 8000;
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(logger('tiny'));
+app.use(require('./routes'));
+
+app.listen(port, function(err){
+    console.log('Listening on port: ' + port);
+});
+
+const dbURI = process.env.DB_URL;
+
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((result) => console.log('connected to db'))
+        .catch((err) => console.log(err));
